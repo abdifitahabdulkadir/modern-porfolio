094bd42edb37503a3071fdc23b7b74a3d8c9243c
abdifitahabdulkadir <abdifitahabdulkadir22@gmail.com>
2024-10-31 22:20:23 +0530

    add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-import NavBar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+
+import NavBar from "../components/Navbar";
 
 export default function Home() {
   return (
     <main className="min-h-screen w-full bg-gray  pt-5 ">
       <NavBar />
-      <div className="px-[80px]"></div>
+      <div className="px-[80px]">
+        <Hero />
+      </div>
     </main>
   );
 }
---
+++ components/Hero.tsx
@@ -0,0 +1,63 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Hero() {
+  return (
+    <section
+      id="home"
+      className="mt-20 flex w-full flex-col-reverse justify-between lg:flex-row"
+    >
+      <div className="flex  w-full flex-col gap-2 lg:max-w-[776px]">
+        <h1 className="heading-1 text-gray-900 ">
+          Hi, I&rsquo;m Abdifitah Abdulkadir 👋
+        </h1>
+        <p className="body-2 w-4/5  text-gray-600">
+          I&rsquo;m a full stack developer (React.js & Node.js) with a focus on
+          creating (and occasionally designing) exceptional digital experiences
+          that are fast, accessible, visually appealing, and responsive. Even
+          though I have been creating web applications for over 7 years, I still
+          love it as if it was something new.
+        </p>
+        <div className="body-2 mt-5 flex flex-col gap-2 text-gray-600">
+          <div className="flex items-center gap-1">
+            <Image
+              src={"/icons/location.svg"}
+              alt="location icon"
+              height={20}
+              width={20}
+              className="invert-0 dark:invert"
+            />
+            <span>Mogadisho, Somalia</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="size-2 rounded-full bg-primary-500"></span>
+            <span>Avaliable for new Projects</span>
+          </div>
+        </div>
+
+        <Link href="/github" className="mt-5 flex items-center gap-2">
+          <Image
+            src={"/icons/github.svg"}
+            alt="github icon"
+            width={30}
+            height={30}
+            loading="eager"
+            priority
+            className="invert-0 dark:invert"
+          />
+        </Link>
+      </div>
+      <div className="relative   h-[320px]  w-[280px] border  border-gray-200 bg-gray-200 ">
+        <Image
+          src={"/icons/man.svg"}
+          alt="developer image"
+          width={100}
+          height={100}
+          loading="eager"
+          priority
+          className="absolute size-full xl:left-[-30px] xl:top-[-30px]"
+        />
+      </div>
+    </section>
+  );
+}
