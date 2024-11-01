02fa8583f7af5f3765733513fbf42bd8c031e6cb
abdifitahabdulkadir <abdifitahabdulkadir22@gmail.com>
2024-11-01 22:15:47 +0530

    fix bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import About from "@/components/About";
+import ApplicatonNavbar from "@/components/ApplicatonNavbar";
 import Contact from "@/components/Contact";
 import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
-import NavBar from "@/components/Navbar";
 import Skills from "@/components/Skills";
 import Testimonials from "@/components/Testimonials";
 import Work from "@/components/Work";
@@ -11,7 +11,7 @@ import Work from "@/components/Work";
 export default function Home() {
   return (
     <main className="min-h-screen w-full bg-gray  pt-5 ">
-      <NavBar />
+      <ApplicatonNavbar />
       <Hero />
       <About />
       <Skills />
