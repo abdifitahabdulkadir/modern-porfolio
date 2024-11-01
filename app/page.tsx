206ade43f6654858e879c641c6caddfe806bbf5a
abdifitahabdulkadir <abdifitahabdulkadir22@gmail.com>
2024-11-01 22:13:21 +0530

    fix not-found module

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,10 @@ import Contact from "@/components/Contact";
 import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
+import NavBar from "@/components/Navbar";
 import Skills from "@/components/Skills";
 import Testimonials from "@/components/Testimonials";
 import Work from "@/components/Work";
-
-import NavBar from "../components/Navbar";
 
 export default function Home() {
   return (
